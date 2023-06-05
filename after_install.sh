d2d24680b7eb405f9559d42cde3e8f86d3266834
trinhld <trinhld@its-global.vn>
2023-06-05 11:00:48 +0700

    commit 3

--- after_install.sh
+++ after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo -i
-chown -R ubuntu:ubuntu /var/www/html/test/storage /var/www/html/test/bootstrap/
-chmod -R 775 /var/www/html/test/storage /var/www/html/test/bootstrap/
+sudo chown -R ubuntu:ubuntu /var/www/html/test/storage /var/www/html/test/bootstrap/
+sudo chmod -R 775 /var/www/html/test/storage /var/www/html/test/bootstrap/
 
 # Generate application key
 php artisan key:generate
